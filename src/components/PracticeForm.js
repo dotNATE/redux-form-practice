be4204d5b907eff14521ec622f7dc6395591ef68
dotNate <nathanielpayne1@googlemail.com>
2022-02-14 11:54:53 +0000

    reduc-form - simple form level submission

--- src/components/PracticeForm.js
+++ src/components/PracticeForm.js
@@ -1,9 +1,37 @@
 import React from "react";
 import Form from "./Form";
+import { SubmissionError } from "redux-form"
 
 const PracticeForm = () => {
-    const submit = (values) => {
-        console.log("values", values);
+    const submit = ({ firstName = "", lastName = "", email = "" }) => {
+        let error = {};
+        let isError = false;
+        
+        if(firstName.trim() === "") {
+            error.firstName = "Required";
+            isError = true;
+        }
+
+        if(firstName.length < 5) {
+            error.firstName = "Min length is 5 characters";
+            isError = true;
+        }
+
+        if(lastName.trim() === "") {
+            error.lastName = "Required";
+            isError = true;
+        }
+
+        if(email.trim() === "") {
+            error.email = "Required";
+            isError = true;
+        }
+
+        if (isError) {
+            throw new SubmissionError(error)
+        } else {
+            console.log("valid values", {firstName, lastName, email});
+        }
     }
 
     return (
